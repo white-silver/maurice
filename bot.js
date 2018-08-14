415ea8f30d877e77c49fed6817ae60d666b1e62a
Ryota Hiyoshi <Ryota@Ryota-no-MacBook-Air.local>
2018-08-14 18:12:16 +0900

    fixed bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,10 @@ client.on('message', message =>
           message.reply( 'おれはモーリス・ホワイトさ！' );
           return;
           }
+          if(message.content === '音楽かけて')
+          {
+          message.guild.channels.get('477793180872343552').send('!play https://www.youtube.com/playlist?list=PLKvNG6pPKW0OiBMgQdhG-4GEaQQKaHmEc&jct=hKDtsT670a_x7NH4CgpYkW8lTjEvXw'\n'!shuffle'\n'!skip'\n'!loopqueue');  
+          }
           if (message.content === 'こんにちは')
           {
           // Send "pong" to the same channel
